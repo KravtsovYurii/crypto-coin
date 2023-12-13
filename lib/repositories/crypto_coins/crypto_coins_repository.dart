732f965a22f149b424353cb8c67b331a6268be72
KravtsovYurii <yurakravtsov31200@gmail.com>
2023-12-13 23:40:19 +0200

    add interface and import GetIt

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -1,9 +1,14 @@
-import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 
-class CryptoCoinsRepository {
+class CryptoCoinsRepository implements InterfaceCoinsRepository {
+  CryptoCoinsRepository({required this.dio});
+
+  final Dio dio;
+
+  @override
   Future<List<CryptoCoin>> getCoinsList() async {
-    final response = await Dio().get(
+    final response = await dio.get(
         'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,SOL,ADA,LINK,DOGE&tsyms=USD');
 
     final data = response.data as Map<String, dynamic>;
